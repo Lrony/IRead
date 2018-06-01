88f74a0c1852e30499348110819a422dbe05c5a3
Lrony <5414115@qq.com>
2018-06-01 17:23:18 +0800

    [About] 新增
    Change-Id: I2764c8b1d6e7f212858f9373e89a85d07974aa0a

---
+++ app/src/main/java/com/lrony/iread/presentation/about/AboutActivity.java
@@ -0,0 +1,15 @@
+package com.lrony.iread.presentation.about;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.lrony.iread.R;
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+}
